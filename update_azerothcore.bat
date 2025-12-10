0f2f7dd624eb970bc1cb992677ff2b10611b39ff
rdev987 <222584535+rdev987@users.noreply.github.com>
2025-12-10 01:32:57 -0500

    Add: additional project files

---
+++ update_azerothcore.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Attempting to pull latest source code
+git pull origin master
+echo done! If you need to update docker see here:
+echo https://www.azerothcore.org/wiki/install-with-docker
+pause
