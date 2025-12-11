355a6a598a7866254993d8edc0c1f5c5ce9febcd
rdev987 <222584535+rdev987@users.noreply.github.com>
2025-12-11 04:22:51 -0500

    Fixed bat

--- update_azerothcore.bat
+++ update_azerothcore.bat
@@ -1,6 +1,12 @@
 @echo off
 echo Attempting to pull latest source code
-git pull origin master
-echo done! If you need to update docker see here:
+:: git pull origin master
+git fetch upstream master
+git merge upstream/master
+
+echo.
+echo done! If there's any conflicts you have to resolve them.
+echo.
+echo If you need to update docker see here:
 echo https://www.azerothcore.org/wiki/install-with-docker
 pause
